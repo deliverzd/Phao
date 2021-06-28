7bf1ed54b6c292a1b5ff2bdda781bcae76ec927f
Mike's <76393175+Dlszer@users.noreply.github.com>
2021-06-28 23:09:53 +0700

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,15 +1,48 @@
 $(document).ready(function(){
+
  const settings = {
+
   "async": true,
+
   "crossDomain": true,
+
   "url": "https://covid19.th-stat.com/json/covid19v2/getTodayCases.json",
+
   "method": "GET"
+
  };
+
  
+
  $.ajax(settings).done(function(response) {
+
   $('#confirmed').text(response.Confirmed);
+
   $('#recovered').text(response.Recovered);
+
   $('#hospitalized').text(response.Hospitalized);
+
   $('#deaths').text(response.Deaths);
+
+ }).fali(function(){
+
+  let failtext = "ขออภัย,ไม่สามารถเเสดงผลได้";
+
+  $('#confirmed').text(failtext);
+
+  $('#recovered').text(failtext);
+
+  $('#hospitalized').text(failtext);
+
+  $('#deaths').text(failtext);
+
  });
+
 });
+
+
+
+
+
+
+
